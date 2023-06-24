7b27b5de575c5d2a6fd3be53a1ef0cfd1dd3ef85
thanakrit phrompong <ketaauto55@gmail.com>
2023-06-24 19:36:41 +0700

    1st commit.....

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeUI extends StatefulWidget {
+  const HomeUI({super.key});
+
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
+
+class _HomeUIState extends State<HomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/views/introduction_ui.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class IntroductionUI extends StatefulWidget {
+  const IntroductionUI({super.key});
+
+  @override
+  State<IntroductionUI> createState() => _IntroductionUIState();
+}
+
+class _IntroductionUIState extends State<IntroductionUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
